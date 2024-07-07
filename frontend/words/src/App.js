b5fc643612162c9eef3f6078b62c5fa926c52d80
rasool <mrasool.mirzaei@gmail.com>
2024-07-08 00:23:15 +0200

    frontend: setup project structure

---
+++ frontend/words/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div class="position-absolute top-50 start-50 translate-middle">
+          <div class="input-group mb-3">
+            <input type="search" 
+            class="form-control" 
+            placeholder="Search for a word" 
+            aria-label="Search" 
+            described="search"></input>
+            <span class="input-group-text" id="search">Search</span>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
